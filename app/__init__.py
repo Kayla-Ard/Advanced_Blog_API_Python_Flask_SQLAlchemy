d815d52493d24d14c673ef8ed54509b1845a3c07
Kayla-Ard <kaylard@yahoo.com>
2024-07-12 20:33:14 -0600

    test

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///blog.db'
 
 db.init_app(app)
 migrate.init_app(app, db)
